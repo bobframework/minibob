89f8d3a0b5ba30eeeb1e11b22c6237b891f61772
ArcadeKasagawa <133647097+ArcadeKasagawa@users.noreply.github.com>
2023-09-25 15:37:28 +0800

    Update lib.js

--- lib.js
+++ lib.js
@@ -8,6 +8,10 @@ export class MiniBob extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        this.subMultilingualComponents = [];
+    }
+    addToSubMultilingualComponents(x) {
+        this.subMultilingualComponents.push(x);
     }
     attributeChangedCallback(name, oldValue, newValue) {
         if (name === "language") {
@@ -17,6 +21,10 @@ export class MiniBob extends HTMLElement {
 
             for (const el of this.shadowRoot.querySelectorAll('[multiurl]')) {
                 el.setAttribute("href", el.getAttribute(`${newValue}-url`));
+            }
+
+            for (const el of this.subMultilingualComponents) {
+                el.setAttribute("language", newValue);
             }
         }
     }
